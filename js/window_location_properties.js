a735cf2f105ed595f01a8d74e03013807f481ea4
milan-infostride <milandeep.bhalla@infostride.com>
2022-06-08 23:34:32 +0530

    windows location properties done

---
+++ js/window_location_properties.js
@@ -0,0 +1,4 @@
+console.log("location href = ",location.href);
+console.log("location hostname = ",location.hostname);
+console.log("location pathname = ",location.pathname);
+console.log("location protocol = ",location.protocol);
